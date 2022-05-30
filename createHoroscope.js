19d34bbc983a990de7fc88700da0fa517f9d031f
Joana Sousa <jbtsousa@gmail.com>
2022-05-30 04:39:40 +0100

    retrieving user data done

--- createHoroscope.js
+++ createHoroscope.js
@@ -2,6 +2,17 @@ var widthC=600;
 var heightC=300;
 let lines, lol, lol2;
 
+queryString = location.search.substring(1);
+
+    var getLink = queryString.split("|");
+
+    var chineseSign = getLink[0];
+    var moon = getLink[1];
+    var partnerSign = getLink[2];
+
+    console.log(chineseSign,moon,partnerSign);
+
+
 function preload(){
 
     lines=loadStrings('data/horoscopes.txt');
@@ -10,12 +21,15 @@ function preload(){
 
 function setup() {
 
+
     lol=random(lines);
     console.log(lol);
 
     //teste RiTa
     lol2 = RiTa.pos(lol); 
     console.log(lol2);
+
+
 
 }
 
---
+++ generate.js
@@ -0,0 +1,30 @@
+
+
+function getChinese(chinese_id) {
+    alert(chinese_id);
+    chinese = chinese_id;
+    return chinese_id;
+
+}
+
+
+function getMoon(moon_id) {
+    alert(moon_id);
+    moon= moon_id;
+    return moon_id;
+}
+
+function getSign(sign_id) {
+    alert(sign_id);
+    partner=sign_id
+    return sign_id;
+}
+
+
+
+
+
+
+
+
+
