9042f271c397dfaa4e89c11656467f06300aa12d
a-catarina-lopes <anacatarina.lopes94@gmail.com>
2022-05-30 14:57:47 +0100

    GivenKeyWord

--- createHoroscope.js
+++ createHoroscope.js
@@ -1,6 +1,5 @@
-var widthC=600;
-var heightC=300;
-let lines, lol, lol2;
+var horoscopes;
+var paragrafo; //é o data do processing
 
 queryString = location.search.substring(1);
 
@@ -15,27 +14,45 @@ queryString = location.search.substring(1);
 
 function preload(){
 
-    lines=loadStrings('data/horoscopes.txt');
-
+    horoscopes=loadStrings('data/horoscopes.txt');
+    
 }
 
 function setup() {
 
+   ///////////////////GivenKeyword a receber "fast"
+    
+   let texto = carregaCont(horoscopes);
+   var rm = criaMod(texto,3);
 
-    lol=random(lines);
-    console.log(lol);
+   RiTa.concordance(texto);
+   let ret = new Array();
+   ret = RiTa.kwic("fast",3);
+   
+   for(let i=0; i<ret.length;i++){
+       paragrafo=paragrafo+ret[i];
+   }
 
-    //teste RiTa
-    lol2 = RiTa.pos(lol); 
-    console.log(lol2);
+   var frases = split(paragrafo,'.');
+   let cump = frases.length;
+   console.log(cump);
+   let a = int(random(0,cump));
 
-
+   var randomFrase;
+   randomFrase = frases[a];
+   console.log(randomFrase);
 
 }
 
+function carregaCont(f){
+    return f.join("\n", loadStrings(f)).toLowerCase();
+}
+
+function criaMod(text, n){
+    let rm = RiTa.markov(n);
+    rm.addText(text);
+    return rm;
+}
+
 function draw() {
-
-    //background(220);
-    //text(lol, 10, 10, 80, 80);
-
 }
