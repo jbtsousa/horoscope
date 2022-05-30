169d5fe18b2491f54a786879920c44c5e8bde06b
Joana Sousa <jbtsousa@gmail.com>
2022-05-30 18:15:26 +0100

    added favicon, lema to screen, tried middleword

--- createHoroscope.js
+++ createHoroscope.js
@@ -3,56 +3,86 @@ var paragrafo; //é o data do processing
 
 queryString = location.search.substring(1);
 
-    var getLink = queryString.split("|");
+var getLink = queryString.split("|");
 
-    var chineseSign = getLink[0];
-    var moon = getLink[1];
-    var partnerSign = getLink[2];
+var chineseSign = getLink[0];
+var moon = getLink[1];
+var partnerSign = getLink[2];
 
-    console.log(chineseSign,moon,partnerSign);
+console.log(chineseSign, moon, partnerSign);
 
-
-function preload(){
-
-    horoscopes=loadStrings('data/horoscopes.txt');
-    
+function preload() {
+    horoscopes = loadStrings('data/horoscopes.txt');
+    fairyTales = loadStrings('data/fairy_tales.txt');
 }
 
 function setup() {
 
-   ///////////////////GivenKeyword a receber "fast"
+    let lema = select('#lema-sign');
+    let dos = select('#dos');
+    let donts = select('#donts');
+    let paragraph = select('#paragraph-chinese');
+
+    let txt_horoscopes = carregaCont(horoscopes);
+    let txt_fairytales = carregaCont(fairyTales);
+
+    var rm = criaMod(txt_horoscopes, 3);
+    var rm_f = criaMod(txt_fairytales, 3);
+
+
+
+    ///////////////////Middle word
+    //ta a dar um erro n sei pq
     
-   let texto = carregaCont(horoscopes);
-   var rm = criaMod(texto,3);
+    var antes= "the";
+    var depois= chineseSign;
+    comp= rm_f.completions(antes,depois);
+    for (i=0; i<comp.length;i++){
+        if (RiTa.isAdjective(comp[i])){
+            console.log(comp[i]);
 
-   RiTa.concordance(texto);
-   let ret = new Array();
-   ret = RiTa.kwic("fast",3);
-   
-   for(let i=0; i<ret.length;i++){
-       paragrafo=paragrafo+ret[i];
-   }
+        }
+    }
 
-   var frases = split(paragrafo,'.');
-   let cump = frases.length;
-   console.log(cump);
-   let a = int(random(0,cump));
 
-   var randomFrase;
-   randomFrase = frases[a];
-   console.log(randomFrase);
+
+    ///////////////////GivenKeyword a receber "fast"
+
+
+    RiTa.concordance(txt_horoscopes);
+    let ret = new Array();
+    ret = RiTa.kwic("fast", 3);
+
+    for (let i = 0; i < ret.length; i++) {
+        paragrafo = paragrafo + ret[i];
+    }
+
+    var frases = split(paragrafo, '.');
+    let cump = frases.length;
+    console.log(cump);
+    let a = int(random(0, cump));
+
+    var randomLema;
+    randomLema = frases[a];
+    console.log(randomLema);
+    lema.html(randomLema,true);
+
+
 
 }
 
-function carregaCont(f){
+function carregaCont(f) {
     return f.join("\n", loadStrings(f)).toLowerCase();
 }
 
-function criaMod(text, n){
+function criaMod(text, n) {
     let rm = RiTa.markov(n);
     rm.addText(text);
     return rm;
 }
 
 function draw() {
+
 }
+
+
