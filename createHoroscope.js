549eace541954a5fe74720af8c033412963a7425
a-catarina-lopes <anacatarina.lopes94@gmail.com>
2022-05-30 23:06:57 +0100

    map1,2,3()

--- createHoroscope.js
+++ createHoroscope.js
@@ -1,6 +1,9 @@
 var horoscopes;
 var paragrafo; //é o data do processing
+let lema, dos, donts, paragraph;
 
+
+//////////////////////////////////////////////////////////////////
 queryString = location.search.substring(1);
 
 var getLink = queryString.split("|");
@@ -9,7 +12,9 @@ var chineseSign = getLink[0];
 var moon = getLink[1];
 var partnerSign = getLink[2];
 
-console.log(chineseSign, moon, partnerSign);
+//console.log(chineseSign, moon, partnerSign);
+
+//////////////////////////////////////////////////////////////////
 
 function preload() {
     horoscopes = loadStrings('data/horoscopes.txt');
@@ -18,40 +23,76 @@ function preload() {
 
 function setup() {
 
-    let lema = select('#lema-sign');
-    let dos = select('#dos');
-    let donts = select('#donts');
-    let paragraph = select('#paragraph-chinese');
+    //declarei la em cima se não ele não identificava "lema" la em baixo
+    lema = select('#lema-sign');
+    dos = select('#dos');
+    donts = select('#donts');
+    paragraph = select('#paragraph-chinese');
+
+    //map1();
+    map2(chineseSign);
+    map3("fast");
+
+}
+
+
+////////////////////////////////////////////////////MAPEAMENTO 1
+///////////////////////////////////////signo >> início de frase >> text marjov e horoscope.txt >> lema
+/*function map1(){
 
     let txt_horoscopes = carregaCont(horoscopes);
-    let txt_fairytales = carregaCont(fairyTales);
+    var rm_2 = criaMod(txt_horoscopes, 3);
 
-    var rm = criaMod(txt_horoscopes, 3);
-    var rm_f = criaMod(txt_fairytales, 3);
+    var gen = rm_2.generate(3);
+    for(let i=0; i<gen.length; i++){
+        print.log(i,gen[i]);
+    }
+
+    let inicio= "you will feel";
+    let comp = new Array();
+    comp = rm_2.completions(splitTokens(inicio));
+    print.log(inicio);
+    //ainda nao acabei de inserir cenas
+
+}*/
 
 
+////////////////////////////////////////////////////MAPEAMENTO 2
+///////////////////////////////////////lua >> middleWord e horoscopes.txt >> verbos e nomes
+function map2(aft){
 
-    ///////////////////Middle word
-    //ta a dar um erro n sei pq
+    let txt_horoscopes = carregaCont(horoscopes);
+    var rm_f = criaMod(txt_horoscopes, 3);
+
+    let bef="the";
+
+    //o erro/problema esta a partir daqui
+    let comp= new Array();
+    comp = rm_f.completions(bef,"pig");
+
     
-    var antes= "the";
-    var depois= chineseSign;
-    comp= rm_f.completions(antes,depois);
-    for (i=0; i<comp.length;i++){
+    for (let i=0; i<comp.length;i++){
         if (RiTa.isAdjective(comp[i])){
             console.log(comp[i]);
 
         }
     }
 
+}
 
 
-    ///////////////////GivenKeyword a receber "fast"
+////////////////////////////////////////////////////MAPEAMENTO 3 - incompleto. falta a camada do fairytales
+///////////////////////////////////////animal chines >> middleWord e fairytales.txt >> adj >> 
+/////////////////////////////////////// >> givenkeyWords e horoscopes.txt >> x frases
+function map3(adj){
+
+    let txt_horoscopes = carregaCont(horoscopes);
+    var rm = criaMod(txt_horoscopes, 3);
 
 
     RiTa.concordance(txt_horoscopes);
     let ret = new Array();
-    ret = RiTa.kwic("fast", 3);
+    ret = RiTa.kwic(adj, 3);
 
     for (let i = 0; i < ret.length; i++) {
         paragrafo = paragrafo + ret[i];
@@ -67,9 +108,9 @@ function setup() {
     console.log(randomLema);
     lema.html(randomLema,true);
 
-
-
 }
+
+////////////////////////////////////////////////////OUTRAS FUNÇÕES
 
 function carregaCont(f) {
     return f.join("\n", loadStrings(f)).toLowerCase();
